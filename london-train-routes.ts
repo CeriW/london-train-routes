a4f7449176a9ae241bbd1401d7b6e7d0d5f490ff
Ceri <ceriwoolway@gmail.com>
2024-05-16 21:29:56 +0100

    Some very basic data setup

---
+++ london-train-routes.ts
@@ -0,0 +1,79 @@
+type LineName = 'bakerloo' | 'central' | 'circle';
+
+const lines = {
+  bakerloo: {
+    stops: [
+      'Harrow & Wealdstone',
+      'Kenton',
+      'South Kenton',
+      'North Wembley',
+      'Wembley Central',
+      'Stonebridge Park',
+      'Harlesden',
+      'Willesden Junction',
+      'Kensal Green',
+      "Queen's Park",
+      'Kilburn Park',
+      'Maida Vale',
+      'Warwick Avenue',
+      'Paddington',
+      'Edgware Road',
+      'Marylebone',
+      'Baker Street',
+      "Regent's Park",
+      'Oxford Circus',
+      'Piccadilly Circus',
+      'Charing Cross',
+      'Embankment',
+      'Waterloo',
+      'Lambeth North',
+      'Elephant & Castle',
+    ],
+  },
+  circle: {
+    stops: [
+      'Hammersmith',
+      'Goldhawk Road',
+      "Shepherd's Bush Market",
+      'Wood Lane',
+      'Latimer Road',
+      'Ladbroke Grove',
+      'Westbourne Park',
+      'Royal Oak',
+      'Paddington',
+      'Edgware Road',
+      'Baker Street',
+      'Great Portland Street',
+      'Euston Square',
+      "King's Cross St. Pancras",
+      'Farringdon',
+      'Barbican',
+      'Moorgate',
+      'Liverpool Street',
+      'Aldgate',
+      'Tower Hill',
+      'Monument',
+      'Cannon Street',
+      'Mansion House',
+      'Blackfriars',
+      'Temple',
+      'Embankment',
+      'Westminster',
+      "St. James's Park",
+      'Victoria',
+      'Sloane Square',
+      'South Kensington',
+      'Gloucester Road',
+      'High Street Kensington',
+      'Notting Hill Gate',
+      'Bayswater',
+    ],
+  },
+};
+
+const isSameLine = (start, end) => {
+  Object.keys(lines).forEach((line) => {
+    console.log(line + ': ' + lines[line]);
+  });
+};
+console.log(isSameLine('Waterloo', 'Baker Street'));
